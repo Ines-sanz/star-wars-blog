f5244a534e91ae0fc6d0601b99d62f5e62748842
Inés Sanz <inessanzm@hotmail.es>
2025-01-11 20:31:22 +0000

    fix: favicon added correctly

--- webpack.dev.js
+++ webpack.dev.js
@@ -6,7 +6,6 @@ const common = require('./webpack.common.js');
 const port = 3000;
 let publicUrl = `ws://localhost:${port}/ws`;
 
-
 module.exports = merge(common, {
     mode: 'development',
     devtool: 'cheap-module-source-map',
@@ -16,11 +15,15 @@ module.exports = merge(common, {
         allowedHosts: "all",
         historyApiFallback: true,
         static: {
-          directory: path.resolve(__dirname, "dist"),
+            directory: path.resolve(__dirname, "dist"),
         },
         client: {
-          webSocketURL: publicUrl
+            webSocketURL: publicUrl
         },
     },
-    plugins: []
+    plugins: [
+        new webpack.DefinePlugin({
+            'process.env.BASENAME': JSON.stringify(process.env.BASENAME || ""),
+        }),
+    ],
 });
